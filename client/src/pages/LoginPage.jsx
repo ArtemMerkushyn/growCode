74c431502c36c440e358abaaf2964211e4dead75
Artem Merkushyn <merkushyndev13@gmail.com>
2023-11-04 15:47:54 +0200

    wrote the login and registration pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <form className='form' onSubmit={(e) => e.preventDefault()}>
+            <h2 className='form__title'>Авторизація</h2>
+            <label className='form__item'>
+                <input 
+                    type="text"
+                    placeholder="Ім'я користувача" 
+                />
+            </label>
+            <label className='form__item'>
+                <input 
+                    type="password"
+                    placeholder="Пароль" 
+                />
+            </label>
+            <div className="form__item">
+                <button 
+                    className='form__btn' 
+                    type='submit'
+                >
+                    Увійти
+                </button>
+                <Link className='form__link' to={'/register'} >Не маю облікового запису</Link>
+            </div>
+        </form>
     );
 }
