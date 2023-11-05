81bbb8fbc2153844eea37d74d1f31fe03c78f8c2
Artem Merkushyn <merkushyndev13@gmail.com>
2023-11-05 08:18:23 +0200

    connected to mongodb database

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+
+const app = express();
+
+//constants
+const PORT = 8080;
+export const secred = 'secredtoken';
+
+//middleware
+app.use(cors()); //для того щоб можно було відправляти з різних ip, запроси до нашого серверу
+app.use(express.json()); //для  того щоб express розумів що дані з фронту будуть приходити у форматі json
+
+//routes
+
+
+async function start() {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://test:testpassword@cluster0.p68m5mc.mongodb.net/`
+        );
+        app.listen(PORT, () => {
+            console.log(`server started on: http://localhost:${PORT}`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
