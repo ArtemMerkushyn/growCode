441dfe5025d69e9f4c113736a15dbcf8bc26d5b3
Artem Merkushyn <merkushyndev13@gmail.com>
2023-11-06 19:33:45 +0200

    implemented getting the user every time the page is refreshed

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,16 @@ import { Outlet } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getMe } from './redux/features/auth/authSlice';
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getMe());
+    }, [dispatch]);
     return (
         <div className="App">
             <NavBar/>
