9c0a05c857dbefc79d344ca9e86d8854698e02ef
Artem Merkushyn <merkushyndev13@gmail.com>
2023-11-08 22:36:13 +0200

    changed baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: 'http://localhost:8080/api'
-    baseURL: 'https://grow-code-l321g5b1j-merkushyndev13s-projects.vercel.app/api'
+    baseURL: 'https://grow-code-api.vercel.app/api'
 });
 
 instance.interceptors.request.use(config => {
