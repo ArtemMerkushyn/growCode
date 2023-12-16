02c9303f6ecf6ac26114b94b1eaa558ad8ebe083
ArtemMerkushyn <merkushyndev13@gmail.com>
2023-12-16 20:56:22 +0200

    implemented the ability to respond to query on client side

--- client/src/pages/QueryPage.jsx
+++ client/src/pages/QueryPage.jsx
@@ -5,13 +5,17 @@ import Moment from 'react-moment';
 import TextareaAutosize from 'react-textarea-autosize';
 import { CountItem } from '../components/CountItem.jsx';
 import { AvatarUsernameLink } from '../components/AvatarUsernameLink.jsx';
-import { HiPencilAlt } from "react-icons/hi";
-import { BsTrash3Fill } from "react-icons/bs";
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteQuery } from '../redux/features/query/querySlice.js';
+import { HiPencilAlt } from "react-icons/hi";
+import { BsTrash3Fill } from "react-icons/bs";
+import { AiOutlineSend } from 'react-icons/ai';
+import { toast } from 'react-toastify';
+import { createReply } from '../redux/features/reply/replySlice.js';
 
 export const QueryPage = () => {
     const [query, setQuery] = useState(null);
+    const [reply, setReply] = useState('');
     const { user } = useSelector((state) => state.auth);
 
     const params = useParams();
@@ -26,6 +30,15 @@ export const QueryPage = () => {
     const handleDeleteQuery = () => {
         dispatch(deleteQuery(params.id));
         navigate('/me');
+    }
+
+    const handleSumitReply = () => {
+        if(!user) toast('Вам потрібно авторизуватися для того щоб відповісти на питання');
+        if(!reply.trim()) toast('Поле для відповіді на питання не може бути порожнім');
+        const queryId = params.id;
+        dispatch(createReply({ queryId, reply }));
+        setReply('');
+        toast('Дякуємо за відповідь!');
     }
 
     useEffect(() => {
@@ -71,6 +84,25 @@ export const QueryPage = () => {
                     </div>
                 )}
             </div>
+            <form 
+                className='post-item__form' 
+                onSubmit={(e) => e.preventDefault()}
+            >
+                <label className='post-item__form-textarea'>
+                    <TextareaAutosize
+                        placeholder='Відповісти на питання'
+                        spellCheck={false}
+                        value={reply}
+                        onChange={e => setReply(e.target.value)}
+                    />
+                </label>
+                <button 
+                    className='post-item__form-btn'
+                    onClick={handleSumitReply}
+                >
+                    <AiOutlineSend />
+                </button>
+            </form>
         </div>
     );
 };
